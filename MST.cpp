89fe8e80469d0d009ca9e967b48240c081f7da63
imry <rodkin.imry@gmail.com>
2025-03-10 22:00:58 +0200

    i

---
+++ MST.cpp
@@ -0,0 +1,5 @@
+//
+// Created by imry on 3/10/25.
+//
+
+#include "MST.h"
---
+++ MST.h
@@ -0,0 +1,16 @@
+//
+// Created by imry on 3/10/25.
+//
+
+#ifndef MST_H
+#define MST_H
+
+
+
+class MST{
+
+};
+
+
+
+#endif //MST_H
